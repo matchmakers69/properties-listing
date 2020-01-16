3bfa3218a06d0cc9cc57d0d16f81cb5b1a700be3
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 22:22:02 +0000

    Created file  SingleAccomodationDetails.test.js

---
+++ src/components/Accomodation/__tests__/SingleAccomodationDetails.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import SingleAccomodationDetails from '../AccomodationType/SingleAccomodationDetails';
+import AccomodationRooms from '../AccomodationType/AccomodationRooms';
+import { findByTestAttr } from '../../../../tests/testUtils';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const match = { params: { id: 'accomodation' } };
+
+const setup = () => {
+  return shallow(<SingleAccomodationDetails match={match} />);
+};
+
+describe('<SingleAccomodationDetails />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  it('includes Preloader', () => {
+    const preloader = findByTestAttr(wrapper, 'pageLoader');
+    expect(preloader.length).toBe(1);
+  });
+
+  it('passes isLoading to Preloader', () => {
+    expect(wrapper.find('[data-test="pageLoader"]').prop('isLoading')).toEqual(
+      true
+    );
+  });
+
+  it('includes AccomodationRooms', () => {
+    expect(wrapper.containsMatchingElement(<AccomodationRooms />)).toEqual(
+      true
+    );
+  });
+
+  it('passes all users to the UsersList', () => {
+    expect(wrapper.find('AccomodationRooms').prop('rooms')).toEqual([
+      'Michal',
+      'Kasia',
+      'Jacek',
+      'Marta',
+      'Tomek',
+      'Ania'
+    ]);
+  });
+
+  it('passes isLoading to Preloader', () => {
+    expect(wrapper.find('[data-test="pageLoader"]').prop('isLoading')).toEqual(
+      true
+    );
+  });
+});
