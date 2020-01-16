3fda82a2004d562b39bfcafd973b7f070989f4d2
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 09:32:44 +0000

    Created component AccomodationTypesDetails

--- src/components/Accomodation/AccomodationDetails/AccomodationTypesDetails.js
+++ src/components/Accomodation/AccomodationDetails/AccomodationTypesDetails.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { getAccomodationData } from '../services/accomodation';
 
 const AccomodationTypesDetails = ({ ...props }) => {
+  const [isLoading, setisLoading] = useState(false);
+  const [accomodation, setAccomodation] = useState([]);
+
+  const fetchAccomodations = useCallback(async () => {
+    setisLoading(true);
+    try {
+      const response = await getAccomodationData();
+      setAccomodation(response);
+      setisLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [setisLoading, setAccomodation]);
+
+  useEffect(() => {
+    fetchAccomodations();
+  }, [fetchAccomodations]);
+
   const id = props.match.params.id;
-  console.log(id);
-  return <>hhh</>;
+
+  console.log(accomodation);
+  return (
+    <>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-12'>title here</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AccomodationTypesDetails;
