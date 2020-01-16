2a636945b730f08eded66647679c3be09df7f873
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 10:41:24 +0000

    Modified AccomodationTypeDetail.js

--- src/components/Accomodation/AccomodationDetails/AccomodationTypeDetail.js
+++ src/components/Accomodation/AccomodationDetails/AccomodationTypeDetail.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Preloader from '../../../components/loader/PageLoader';
+import styles from './Styles.module.scss';
+import GlobalContainer from '../../../styles/GlobalContainer';
 
 const AccomodationTypeDetail = ({
   accomodationTypes,
@@ -17,15 +19,21 @@ const AccomodationTypeDetail = ({
     }
   }, [accomodationTypes, propertiesTypeId]);
 
-  console.log(propertyType);
+  const { type, accomodations } = propertyType;
 
   return (
     <Preloader isLoading={isLoading}>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-xs-12'>title here</div>
+      <GlobalContainer>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xs-12'>
+              <header className={styles.propertyTypeHeader}>
+                <h1 className='h1'>{type}</h1>
+              </header>
+            </div>
+          </div>
         </div>
-      </div>
+      </GlobalContainer>
     </Preloader>
   );
 };
