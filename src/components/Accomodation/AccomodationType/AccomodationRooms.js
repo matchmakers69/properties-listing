02084c06e59a5c84c81836b68d153537eb223822
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 17:52:49 +0000

    Modified component AccomodationRooms, created function getRoomsByAvailability

--- src/components/Accomodation/AccomodationType/AccomodationRooms.js
+++ src/components/Accomodation/AccomodationType/AccomodationRooms.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { getAccomodationAvailableData } from '../services/accomodation_available';
 
 const AccomodationRooms = ({ rooms }) => {
-  console.log(rooms);
+  const [availableRooms, setAvailableRooms] = useState([]);
+  const fetchAvailableRooms = useCallback(async () => {
+    try {
+      const rooms_available = await getAccomodationAvailableData();
+      setAvailableRooms(rooms_available);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAvailableRooms();
+  }, [fetchAvailableRooms]);
+
+  const getRoomsByAvailability = () => {
+    let allRooms = [];
+    allRooms = rooms.map(item => {
+      const filteredByAvailability = availableRooms.filter(x => {
+        return x.id === item.id;
+      });
+
+      return { room: item, availability: filteredByAvailability };
+    });
+    return allRooms;
+  };
+  const roomsAvailableById = getRoomsByAvailability();
+
   return <></>;
 };
 
