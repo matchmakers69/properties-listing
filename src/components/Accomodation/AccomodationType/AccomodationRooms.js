e1ec8bee73d19a39759e7b3ec0aaad4fb7048e25
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 19:03:30 +0000

    Modified AccomodationRooms component

--- src/components/Accomodation/AccomodationType/AccomodationRooms.js
+++ src/components/Accomodation/AccomodationType/AccomodationRooms.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { getAccomodationAvailableData } from '../services/accomodation_available';
 import { getRoomsByAvailability } from '../services/getRoomsByAvailability';
+import _isEmpty from 'lodash/isEmpty';
+import styles from './Styles.module.scss';
 
 const AccomodationRooms = ({ rooms }) => {
   const [availableRooms, setAvailableRooms] = useState([]);
@@ -19,8 +21,48 @@ const AccomodationRooms = ({ rooms }) => {
   }, [fetchAvailableRooms]);
 
   const roomsAvailableById = getRoomsByAvailability(rooms, availableRooms);
-  console.log(roomsAvailableById);
-  return <></>;
+
+  return (
+    <div>
+      <h3 className={styles.roomTypeTitle}>Room type</h3>
+      <ul className={styles.typesList}>
+        {!_isEmpty(roomsAvailableById) ? (
+          roomsAvailableById.map((item, index) => {
+            const { room, availability } = item;
+            const {
+              id,
+              max_occupancy,
+              min_occupancy,
+              number_of_nights,
+              type,
+              name,
+              price
+            } = room;
+            const priceRoom = !_isEmpty(price)
+              ? price.price
+              : 'Sorry there is no price to be displayed!';
+            console.log(priceRoom);
+            return (
+              <li key={id}>
+                <div className={styles.typesWrapper}>
+                  <h4 className={styles.typesHeader}>
+                    Name: <p>{name}</p>
+                  </h4>
+                </div>
+                <div className={styles.typesWrapper}>
+                  <h4 className={styles.typesHeader}>
+                    Type: <p>{type}</p>
+                  </h4>
+                </div>
+              </li>
+            );
+          })
+        ) : (
+          <p>No available room </p>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default AccomodationRooms;
