bc37a0efbd71a192b33abb29fa4e6b460488f6c4
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 17:18:15 +0000

    Modified Layout.js added PropTypes

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Layout = ({ ...props }) => {
+const Layout = ({ children }) => {
   return (
     <div role='main' data-test='kaboodle-app' className='main'>
-      {props.children}
+      {children}
     </div>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.array])
 };
 
 export default Layout;
