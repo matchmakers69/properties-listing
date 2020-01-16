49c82eee2fa463dd2eb6cab126061911f60a0363
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 10:14:35 +0000

    Modified AccomodationTypesDetails added Consumer

--- src/components/Accomodation/AccomodationDetails/AccomodationTypesDetails.js
+++ src/components/Accomodation/AccomodationDetails/AccomodationTypesDetails.js
@@ -1,36 +1,22 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { getAccomodationData } from '../services/accomodation';
+import React from 'react';
+import { AccomodationContext } from '../../../context/AccomodationContext';
+import AccomodationTypeDetail from './AccomodationTypeDetail';
 
-const AccomodationTypesDetails = ({ ...props }) => {
-  const [isLoading, setisLoading] = useState(false);
-  const [accomodation, setAccomodation] = useState([]);
-
-  const fetchAccomodations = useCallback(async () => {
-    setisLoading(true);
-    try {
-      const response = await getAccomodationData();
-      setAccomodation(response);
-      setisLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [setisLoading, setAccomodation]);
-
-  useEffect(() => {
-    fetchAccomodations();
-  }, [fetchAccomodations]);
-
+const AccomodationTypesDetails = props => {
   const id = props.match.params.id;
 
-  console.log(accomodation);
   return (
-    <>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-xs-12'>title here</div>
-        </div>
-      </div>
-    </>
+    <AccomodationContext.Consumer>
+      {({ accomodationTypes, isLoading }) => (
+        <>
+          <AccomodationTypeDetail
+            accomodationTypes={accomodationTypes}
+            propertiesTypeId={id}
+            isLoading={isLoading}
+          />
+        </>
+      )}
+    </AccomodationContext.Consumer>
   );
 };
 
