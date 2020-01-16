a32c5ab60767a77fde48c6ed75f95cf904c307ec
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-16 12:13:47 +0000

    Modified Routes - added route for single accomodation

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AccomodationTypes from '../views/AccomodationTypes';
 import AccomodationTypesDetails from '../components/Accomodation/AccomodationDetails/AccomodationTypesDetails';
+import SingleAccomodation from '../components/Accomodation/AccomodationType/SingleAccomodation';
 
 export const Routes = () => {
   return (
@@ -11,6 +12,11 @@ export const Routes = () => {
         exact
         path='/accomodation-type/:id'
         component={AccomodationTypesDetails}
+      />
+      <Route
+        exact
+        path='/accomodation-type/accomodation/:id'
+        component={SingleAccomodation}
       />
     </Switch>
   );
