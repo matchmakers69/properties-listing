5e5f8f9bfece50641974fba65c84d3cbb6f56f7d
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-17 15:42:34 +0000

    Modified Header.js - added AccomodationContext.Consumer

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './Styles.module.scss';
 import NavigationBar from './NavigationBar';
+import { AccomodationContext } from '../../context/AccomodationContext';
 
 const Header = () => (
-  <header className={styles.mainHeader}>
-    <NavigationBar />
-  </header>
+  <AccomodationContext.Consumer>
+    {({ accomodationTypes, isLoading }) => (
+      <header className={styles.mainHeader}>
+        <NavigationBar
+          accomodationTypes={accomodationTypes}
+          isLoading={isLoading}
+        />
+      </header>
+    )}
+  </AccomodationContext.Consumer>
 );
 
 export default Header;
