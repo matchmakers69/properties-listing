641de7ebbe0d251377c4561ac4bbff17f3f7f011
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-01-17 14:56:57 +0000

    Created Header and NavigationBar components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Routes } from './routes';
 import './style.scss';
 import GlobalStyle from './styles/GlobalStyle';
 import Layout from './layout/Layout';
+import Header from './layout/Header/Header';
 import { AccomodationProvider } from './context/AccomodationContext';
 
 const App = () => (
   <AccomodationProvider>
     <BrowserRouter>
+      <Header />
       <Layout>
         <GlobalStyle />
         <Routes />
---
+++ src/layout/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Styles.module.scss';
+
+const Header = () => <header className={styles.mainHeader}></header>;
+
+export default Header;
